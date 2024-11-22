edd948327ded23875ed15dad6424884a5d6318bf
radimous <jurda92@gmail.com>
2024-11-22 16:46:30 +0100

    mod v1

---
+++ src/main/java/com/radimous/smartresearchcost/Smartresearchcost.java
@@ -0,0 +1,30 @@
+package com.radimous.smartresearchcost;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("smartresearchcost")
+public class Smartresearchcost {
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Smartresearchcost() {
+
+    }
+
+}
---
+++ src/main/java/com/radimous/smartresearchcost/mixin/MixinResearchTree.java
@@ -0,0 +1,53 @@
+package com.radimous.smartresearchcost.mixin;
+
+import iskallia.vault.research.ResearchTree;
+import iskallia.vault.research.type.Research;
+import iskallia.vault.util.PlayerReference;
+import iskallia.vault.world.data.PlayerResearchesData;
+import net.minecraftforge.server.ServerLifecycleHooks;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(value = ResearchTree.class, remap = false)
+public abstract class MixinResearchTree {
+    @Shadow @Final protected List<PlayerReference> researchShares;
+
+    @Shadow public static boolean isPenalty;
+
+    @Shadow public abstract float getTeamResearchCostIncreaseMultiplier();
+
+
+    // Do not increase the cost if another players in group have it researched
+    // If anything goes wrong, do not lower the cost and return the original value
+    @Redirect(method = "getResearchCost", at = @At(value = "INVOKE", target = "Liskallia/vault/research/ResearchTree;getTeamResearchCostIncreaseMultiplier()F"))
+    private float doNotIncreaseIfAlreadyResearched(ResearchTree instance, Research research) {
+        if (research == null || isPenalty) {
+            return instance.getTeamResearchCostIncreaseMultiplier();
+        }
+        int notUnlocked = 0;
+
+        var server = ServerLifecycleHooks.getCurrentServer();
+        if (server == null) {
+            return instance.getTeamResearchCostIncreaseMultiplier();
+        }
+        var ow = server.overworld();
+        if (ow == null) {
+            return instance.getTeamResearchCostIncreaseMultiplier();
+        }
+        for (PlayerReference playerReference : this.researchShares) {
+            var playerId = playerReference.getId();
+            if (playerId == null) {
+                return instance.getTeamResearchCostIncreaseMultiplier();
+            }
+            if (!PlayerResearchesData.get(ow).getResearches(playerId).isResearched(research)) {
+                notUnlocked++;
+            }
+        }
+        return notUnlocked * 0.5F;
+    }
+}
